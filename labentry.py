3d4b1b8bd12e6c03f2c2abe26e8e8881903423d0
evangowan <evangowan@gmail.com>
2022-10-30 21:15:53 +0900

    the python database entry script has been tested on a bunch of Pennsylvania date lists, and appears to be working

--- labentry.py
+++ labentry.py
@@ -35,6 +35,16 @@ class labentry:
 
 		file.close()
 
+		self.entry_options = [
+			{'number': 1, 'option': 'edit Lab Name'},
+			{'number': 2, 'option': 'edit country'},
+			{'number': 3, 'option': 'edit lab type'},
+			{'number': 4, 'option': 'edit year opened'},
+			{'number': 5, 'option': 'add fractionation notes'},
+			{'number': 6, 'option': 'add uncertainty notes'},
+			{'number': 7, 'option': 'add a reference'},
+		  ]
+
 
 
 	def add_lab_name(self):
@@ -100,6 +110,122 @@ class labentry:
 			print("invalid input, nothing added")
 
 
+	def add_fractionation_note(self):
+		
+
+		fractionation_note = self.fractionation_correction.copy()
+
+		print("Input the year of the publication")
+		try:
+			year = int(input(">>> "))
+
+			if year > 1600 and year < 2200: # hey, I am being conservative here
+				success = True
+				fractionation_note['year'] = year
+			else:
+				print("I suspect the year has been entered incorrectly")
+				success = False
+		except ValueError:
+			print("Error in entering the year, aborting")
+			success = False
+
+		if success:
+			print("Enter the notes about the fractionation:")
+			fractionation_note['notes'] = input(">>> ")
+
+			print("Enter the bibtex key for the reference:")
+			fractionation_note['reference'] = input(">>> ")
+			self.labinfo['fractionation_notes'].append(fractionation_note)
+
+			self.labinfo['fractionation_notes'] = sorted(self.labinfo['fractionation_notes'], key=lambda k: k['year']) # sort the entries by year
+
+		return success
+
+	def add_uncertainty_note(self):
+		
+
+		uncertainty_note = self.uncertainty.copy()
+
+		print("Input the year of the publication")
+		try:
+			year = int(input(">>> "))
+
+			if year > 1600 and year < 2200: # hey, I am being conservative here
+				success = True
+				uncertainty_note['year'] = year
+			else:
+				print("I suspect the year has been entered incorrectly")
+				success = False
+		except ValueError:
+			print("Error in entering the year, aborting")
+			success = False
+
+		if success:
+			print("Enter the notes about the uncertainty:")
+			uncertainty_note['notes'] = input(">>> ")
+
+			print("Enter the bibtex key for the reference:")
+			uncertainty_note['reference'] = input(">>> ")
+			self.labinfo['uncertainty_notes'].append(uncertainty_note)
+
+			self.labinfo['uncertainty_notes'] = sorted(self.labinfo['uncertainty_notes'], key=lambda k: k['year']) # sort the entries by year
+
+		return success
+
+
+	def add_reference(self):
+		
+
+		reference_note = self.reference.copy()
+
+		print("Input the year of the publication")
+		try:
+			year = int(input(">>> "))
+
+			if year > 1600 and year < 2200: # hey, I am being conservative here
+				success = True
+				reference_note['year'] = year
+			else:
+				print("I suspect the year has been entered incorrectly")
+				success = False
+		except ValueError:
+			print("Error in entering the year, aborting")
+			success = False
+
+		if success:
+			print("Enter the notes about the reference:")
+			reference_note['notes'] = input(">>> ")
+
+			print("Enter the lowest lab number found in this reference (enter to skip):")
+			try:
+				low_number = int(input(">>> "))
+				if low_number < 1:
+					print("Invalid number")
+				else:
+					reference_note['low_number'] = low_number
+			except ValueError:
+				print("Skipped adding lowest lab number")
+
+			print("Enter the highest lab number found in this reference (enter to skip):")
+			try:
+				high_number = int(input(">>> "))
+				if high_number < 1:
+					print("Invalid number")
+				else:
+					reference_note['high_number'] = high_number
+			except ValueError:
+				print("Skipped adding lowest lab number")
+
+
+			print("Enter the bibtex key for the reference:")
+			reference_note['reference'] = input(">>> ")
+			self.labinfo['references'].append(reference_note)
+
+			self.labinfo['references'] = sorted(self.labinfo['references'], key=lambda k: k['year']) # sort the entries by year
+
+		return success
+
+
 	def check_labcode(self, lab_id):
 
 		if lab_id in self.labcodes:
@@ -135,6 +261,8 @@ class labentry:
 
 		file = open(self.labcode_file, 'w')
 
+		self.labcodes.sort()
+
 		yaml.dump(self.labcodes, file, default_flow_style=False, allow_unicode=True)
 		file.close()
 
@@ -147,6 +275,30 @@ class labentry:
 
 		yaml.dump(self.labinfo, file, default_flow_style=False, allow_unicode=True)
 		file.close()
+
+	def print_options(self):
+
+		for option in self.entry_options:
+			print(f"{option['number']}) {option['option']}")
+
+	def select_option(self):
+
+		print("Select option")
+
+		self.print_options()
+
+		try:
+
+			option_number = int(input(">>> "))
+			found = False
+			for option in self.entry_options:
+				if option_number == option['number']:
+					found = True
+
+		except ValueError:
+			found = False
+
+		return found, option_number
 
 
 	def edit_entry(self,lab_id=''):
@@ -178,7 +330,35 @@ class labentry:
 
 			write_labinfo = True
 	
-		print(self.labinfo)
+		found, option_number = self.select_option()
+
+		if found:
+			if option_number == 1:
+				self.add_lab_name()
+				write_labinfo = True
+			elif option_number == 2:
+				self.add_country()
+				write_labinfo = True
+			elif option_number == 3:
+				self.add_lab_type()
+				write_labinfo = True
+			elif option_number == 4:
+				self.add_year_opened()
+				write_labinfo = True
+			elif option_number == 5:
+				success = self.add_fractionation_note()
+				if success:
+					write_labinfo = True
+			elif option_number == 6:
+				success = self.add_uncertainty_note()
+				if success:
+					write_labinfo = True
+			elif option_number == 7:
+				success = self.add_reference()
+				if success:
+					write_labinfo = True
+			else:
+				print("nothing entered, exiting")
 
 		if not found_labcode:
 			self.write_labcode_file()
